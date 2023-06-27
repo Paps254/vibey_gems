0ec834f6be5bd6970bc2516177d97e94e805e308
KUNI <kunijeremy@gmail.com>
2023-06-27 10:26:52 +0300

    components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+ 
+function Footer() {
+    return (
+        <div className="footer">
+            <p>Copyright &copy; 2023 Codr Kai Tutorials</p>
+        </div>
+    )
+}
+
+export default Footer
